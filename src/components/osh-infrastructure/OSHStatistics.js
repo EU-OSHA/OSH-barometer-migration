504100d76028bf6cc07eddee1f0ba8b2b097dde6
manuel-daCosta <manueldacosta@bilbomatica.es>
2021-03-18 10:17:39 +0100

    feat: added component logic, added cards - pagination - filters components and api calls

--- src/components/osh-infrastructure/OSHStatistics.js
+++ src/components/osh-infrastructure/OSHStatistics.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import ReactHtmlParser from 'react-html-parser';
 
 import AdviceSection from '../common/AdviceSection';
 import Methodology from '../common/Methodology';
-import Pagination from '../pagination/Pagination';
-import { getOSHCountries, getOSHStatistic } from '../../api';
-import Cards from '../cards/Cards';
+import Cards from '../common/cards/Cards';
+import Pagination from '../common/pagination/Pagination';
+import SelectFilters from '../common/select-filters/SelectFilters';
+import { getOSHCountries, getOSHData } from '../../api';
 
 const literals = require('../../model/Literals.json');
 
@@ -18,56 +18,73 @@ class OSHStatistics extends Component
 			countries: [],
 			matrixPageData: [],
 			pageOfItems: [],
-			institutionTypes: [
-				{id: '', literal: ''},
-				{id: '', literal: ''},
-				{id: '', literal: ''}
-			],
-			countryDropdownRef: React.createRef(),
-			institutionDropdownRef: React.createRef(),
-			searchBarText: '',
 			isFetching: false,
 			filters: {
 				countries: [],
-				checks: [
-					{id: '1', boolean: false},
-					{id: '2', boolean: false},
-					{id: '3', boolean: false},
-				],
+				checks: [],
 				searchBar: ''
 			},
 		}
 	}
 
-	// Handle dropdown when clicked outside of their container
-	onHandleDropdown = e => {
-		if (this.state.countryDropdownRef.current && !this.state.countryDropdownRef.current.contains(e.target)) {
-			this.setState({ isCountryDropdown: false });
-		}
-
-		if(this.state.institutionDropdownRef.current && !this.state.institutionDropdownRef.current.contains(e.target)) {
-			this.setState({ isInstitutionDropdown: false });
+	handleCallbackCountry = (countryCallback) => {
+		const countryFilter = this.state.filters.countries;
+		if (countryCallback != this.state.filters.countries.find((code) => code == countryCallback)) {
+			this.setState({ filters: {...this.state.filters, countries: [...countryFilter, countryCallback]}});
+		} else {
+			const index = this.state.filters.countries.findIndex((code) => code == countryCallback);
+			const newCountryFilters = this.state.filters.countries;
+			newCountryFilters.splice(index, 1);
+			this.setState({filters: {...this.state.filters, countries: newCountryFilters}});
 		}
 	}
 
-	// On selected country
-	onSelectedCountry = (country) => {
+	handleCallbackCategory = (categoryCallback) => {
+		const checkFilter = this.state.filters.checks.find((data) => data.id == categoryCallback.id);
+		const checks = this.state.filters.checks;
+		if (!checkFilter) {
+			this.setState({ filters: {...this.state.filters, checks: [...checks, { ...categoryCallback, check: true }]}});
+		} else {
+			const index = this.state.filters.checks.findIndex((data) => data.id == categoryCallback.id);
+			const newArray = this.state.filters.checks;
+			newArray.splice(index, 1);
+			this.setState({ filters: { ...this.state.filters, checks: newArray } });
+		}
+	}
+
+	// Handles callback for the search bar on filters component
+	handleCallbackSearch = (searchCallback) => {
+		this.setState({ filters: { ...this.state.filters, searchBar: searchCallback } })
+	}
+
+	//when country is selected
+	onSelectCountryTag = (country) => {
 		return () => {
-			const countryState = this.state.filters.countries;
-			if (country != this.state.filters.countries.find((code) => code == country )) {
-				this.setState({filters: {...this.state.filters, countries: [...countryState, country]}});
-			} else {
-				const index = this.state.filters.countries.findIndex((code) => code == country);
-				const newArray = this.state.filters.countries;
-				newArray.splice(index, 1);
-				this.setState({filters: {...this.state.filters, countries: newArray}});
-			}
+			const countryIndex = this.state.filters.countries.findIndex((code) => code == country);
+			const newArray = this.state.filters.countries;
+			newArray.splice(countryIndex, 1);
+			this.setState({filters: {...this.state.filters, countries: newArray}});
 		}
 	}
 
-	// Dropdown for country selector
-	onClickCountryDropdown = () => {
-		this.setState({ isCountryDropdown: !this.state.isCountryDropdown });
+	//when country is selected
+	onSelectCountryTag = (country) => {
+		return () => {
+			const countryIndex = this.state.filters.countries.findIndex((code) => code == country);
+			const newArray = this.state.filters.countries;
+			newArray.splice(countryIndex, 1);
+			this.setState({filters: {...this.state.filters, countries: newArray}});
+		}
+	}
+
+	//whe category is selected
+	onSelectInstitutionTag = (institutionId) => {
+		return () => {
+			const institutionIndex = this.state.filters.checks.findIndex((institution) => institution.id == institutionId);
+			const newArray = this.state.filters.checks;
+			newArray.splice(institutionIndex, 1);
+			this.setState({ filters: { ...this.state.filters, checks: newArray } })
+		}
 	}
 
 	// Handles change of page when clicked on the next or prev
@@ -78,13 +95,12 @@ class OSHStatistics extends Component
 	componentDidMount() {
 		this.setState({ ...this.state, isFetching: true});
 		try {
-			getOSHCountries(['UK', 'EU28'])
+			getOSHCountries('MATRIX_STATISTICS' ,['UK'])
 				.then((res) => {
 					this.setState({ countries: res.resultset })
 				});
-			getOSHStatistic()
+			getOSHData('MATRIX_STATISTICS')
 				.then((res) => {
-					console.log('data:', res.resultset);
 					this.setState({ matrixPageData: res.resultset })
 				});
 		} catch(error) {
@@ -92,11 +108,22 @@ class OSHStatistics extends Component
 		} finally {
 			this.setState({ ...this.state, isFetching: false })
 		}
+
 	}
 
 	componentDidUpdate(prevProps, prevState) {
 		if (prevState.filters != this.state.filters) {
-			console.log(this.state.fil)
+			this.setState({ ...this.state, isFetching: true })
+			try {
+				getOSHData('MATRIX_STATISTICS', this.state.filters)
+					.then((res) => {
+						this.setState({ matrixPageData: res.resultset })
+					})
+			} catch (error) {
+				console.log('Error Fetching data', error)
+			} finally {
+				this.setState({ ...this.state, isFetching: false })
+			}
 		}
 	}
 
@@ -107,66 +134,43 @@ class OSHStatistics extends Component
 				<AdviceSection />
 
 				<section className="container">
-					<form className="row block--filter--wrapper">
-						{/*  COUNTRY FILTER */}
-						<div id="filter2" className={`filter--dropdown--wrapper ${this.state.isCountryDropdown ? 'viewOptions' : null}`} tabIndex="9">
-							<div className="filter--dropdown--list" ref={this.state.countryDropdownRef}>
-								<p className="option-title" onClick={this.onClickCountryDropdown} >{this.props.literals.L20630}</p>
-								<ul className="filter--dropdown--options">
-									{this.state.countries.map((country) => (
-										<li key={country.code} onClick={this.onSelectedCountry(country.code)} >
-											<input type="checkbox" checked={this.state.filters.countries.includes(country.code)} readOnly />
-											<label >{`(${country.code})`} {country.name}</label>
-										</li>
-									))}
-								</ul>
-							</div>
-						</div> 
-						{/*  CATEGORY TYPE FILTER */}
-						<div id="filter1" className="filter--dropdown--wrapper" tabIndex="8">
-							<div className="filter--dropdown--list">
-								<p className="option-title" ng-click="openSelect($event)" >{this.props.literals.L20651}</p>
-								<ul className="filter--dropdown--options">
-									<li>
-										<input id='category-filter-1' type="checkbox" ng-click="toggleCategoryClick($event);openSelect($event)" tabIndex="-1"/>
-										<label htmlFor='category-filter-1' >{this.props.literals.L20714}</label>
-									</li>
-									<li>
-										<input id='category-filter-2' type="checkbox" ng-click="toggleCategoryClick($event);openSelect($event)" tabIndex="-1"/>
-										<label htmlFor='category-filter-2' >{this.props.literals.L20715}</label>
-									</li>
-									<li>
-										<input id='category-filter-3' type="checkbox" ng-click="toggleCategoryClick($event);openSelect($event)" tabIndex="-1"/>
-										<label htmlFor='category-filter-3' >{this.props.literals.L20716}</label>
-									</li>
-								</ul>
-							</div>
-						</div>
-						{/*  SEARCH FILTER */}
-						<div className="filter-text">
-							<input ng-keypress="clickEnter($event)" ng-focus="closeSelect($event)" id="search-input" type="text" tabIndex="7" placeholder={this.props.literals.L378} />
-							<button onClick="clickEnter($event)"  id="policy-search" type="button">
-							<i className="fa fa-search" aria-hidden="true"></i>
-							</button>
-						</div>
-					</form>
+
+					<SelectFilters 
+						selectCountries={this.state.countries} 
+						literals={literals} 
+						onClickCountry={this.handleCallbackCountry}
+						onClickInstitution={this.handleCallbackCategory}
+						onSearchbarClick={this.handleCallbackSearch}
+						selectedFilters={this.state.filters}
+						filterCategory={'category'}
+					/>
+
 					<div className="container">
-						{/* CONTENT */}
-						<div className="selected--tags-wrapper"></div>
+						<div className="selected--tags-wrapper">
+							{this.state.filters && (
+								<div>
+									{this.state.filters.countries.map((country) => <span key={country} className="selected-tag" onClick={this.onSelectCountryTag(country)}>{country}</span>)}
+									{this.state.filters.checks.map((category) => {
+										if (category.check) {
+											return <span key={category.id} className="selected-tag" onClick={this.onSelectInstitutionTag(category.id)} >{this.props.literals[`L${category.literal}`]}</span>
+										}
+									})}
+								</div>
+							)}
+						</div>
 						
-						{/** Cards Component */}
+					{/** Cards Component */}
 					<div className="matrix--elements--wrapper">
 						{this.state.pageOfItems.length > 0 ? (
 							this.state.pageOfItems.map((data, index) => {
 								const id = `${index}-${data.country.code}`
-								// console.log(data)
 								return <Cards key={id} countryData={data} literals={literals} />
 							})
 						) : (<span>{this.props.literals.L20706}</span>)}
 					</div>
 						
-						{/* PAGINATION */}
-						<Pagination items={this.state.matrixPageData} onChangePage={this.onChangePage} />
+					{/* PAGINATION */}
+					<Pagination items={this.state.matrixPageData} onChangePage={this.onChangePage} />
 
 					</div>
 				</section>
