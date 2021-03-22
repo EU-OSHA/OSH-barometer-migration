b2f2214e7452cad4b4276f52ecec33066532a444
Jose Ignacio Lorenzo Caminero <jilorenzo@bilbomatica.es>
2021-03-22 16:09:14 +0100

    footer of the regulation page displays always at the botoom

--- src/components/osh-steering/Regulation.js
+++ src/components/osh-steering/Regulation.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import ReactHtmlParser from 'react-html-parser';
+import $ from "jquery";
 import AdviceSection from '../common/AdviceSection';
 
 class Regulation extends Component
 {
+	componentDidMount(){
+        // footer at te bottom side for coming soon status
+        if( $('.coming-soon').length > 0 ){
+			var h = Number( $('header').height() + 244 + 130  ) ;
+			var w = $(window).height();
+			var f = $('footer').height();
+			$('.coming-soon').css('height', w-h-f);
+		  }else {
+			 $('.coming-soon').remove();
+		  }
+		  
+		  $(window).on("resize",function(e){
+			// footer at te bottom side for coming soon status
+			if( $('.coming-soon').length > 0 ){
+			  var h = Number( $('header').height() + 244 + 130  ) ;
+			  var w = $(window).height();
+			  var f = $('footer').height();
+			  $('.coming-soon').css('height', w-h-f);
+			}else {
+			   $('.coming-soon').remove();
+			}
+		  });
+	}
 	render()
 	{
 		return(
