bc2061ee0f164e18b6998f095ae635cb32280764
manuel-daCosta <manueldacosta@bilbomatica.es>
2021-06-01 12:30:02 +0200

    fix: added new function for custom export using xlsx from utils

--- src/components/common/charts/MentalRiskCharts.js
+++ src/components/common/charts/MentalRiskCharts.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official';
 import { getChartData, getDatasourceAndDates } from '../../../api';
-import { largeSize } from '../utils/chartConfig';
-import ReactHtmlParser from 'react-html-parser'
-import zipcelx from 'zipcelx';
+import { largeSize, xlsxCustomExport } from '../utils/chartConfig';
 
 require('highcharts/modules/exporting')(Highcharts);
 require('highcharts/modules/export-data')(Highcharts);
@@ -97,43 +95,7 @@ class MentalRiskCharts extends Component {
                     menuItemDefinitions: {
                         "downloadXLS": {
                             onclick: function() {
-                                const series = [{value: 'Category', type:'string'}];
-                                const auxSeries = this.series;
-                                const categories = [];
-                                const seriesData = [];
-
-                                console.log('auxSeries', auxSeries)
-
-                                auxSeries.map((element) => {
-                                    series.push({value: element.name, type: typeof element.name})
-                                })
-
-                                auxSeries.forEach((element) => element.data.map(
-                                    (innerEl) => {
-                                        categories.push({value: innerEl.options.name, type: typeof innerEl.options.name});
-                                        seriesData.push({value: innerEl.options.y, type: typeof innerEl.options.y});
-                                        // const arrayObj1 = {value: innerEl.options.name, type: typeof innerEl.options.name};
-                                        // const arrayObj2 = {value: innerEl.options.y, type: typeof innerEl.options.y};
-                                        // const obj2 = ;
-                                        // categories.push(Object.assign({...arrayObj1}, arrayObj2))
-                                        // categories = [{value: innerEl.options.name, type: typeof innerEl.options.name}, {value: innerEl.options.y, type: typeof innerEl.options.y}]
-                                    }
-                                ))
-
-                                console.log('categories', categories)
-                                console.log('seriesData', seriesData)
-                                console.log('series', series);
-                                const config = {
-                                    filename: this.title.textStr.replace(/ /g, '_'),
-                                    sheet: {
-                                        data: [
-                                            series,
-                                            categories,
-                                            seriesData
-                                        ]
-                                    }
-                                }
-                                zipcelx(config)
+                                xlsxCustomExport('Category', this.series, this.title.textStr);
                             }
                         }
                     }
