237dbc375e64474a96d242d918168a68e1b8f766
manuel-daCosta <manueldacosta@bilbomatica.es>
2021-06-10 11:35:35 +0200

    feat: creation of osh culture full country report component

---
+++ src/components/about-tool/country-report-sections/OshCultureReport.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import HealthAwareness from '../../common/charts/HealthAwareness';
+
+import { oshCulture } from '../../../model/subMenuTabs';
+
+class OshCultureReport extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            country: this.props.country,
+            chartLegend: ''
+        }
+    }
+
+    callbackChartLegend = (legend) => {
+        // this.setState({ chartLegend: legend })
+		// console.log('chart legend', legend)
+	}
+
+    render() {
+        return (
+            <React.Fragment>
+                {oshCulture.map(element => element.chartType.map((chart) => {
+                    return (
+                        <div key={chart} className="box-rounded">
+                            <h2 className="card-block--chart--wrapper" >{this.props.literals[`L${element.literalTab}`]}</h2>
+                            <HealthAwareness 
+                                literals={this.props.literals}
+                                country={this.state.country}
+                                chartType={[chart]}
+                                colors={['#7b7b7d', '#cbe2e3','#449fa2']}
+                                type={'column'}
+                                callbackLegend={this.callbackChartLegend}
+                                fullCountryReport={true}
+                            />
+                            <p>{this.props.literals[`L${chart.legend}`]}</p>
+                        </div>
+                    )
+                }))}
+            </React.Fragment>
+        );
+    }
+}
+
+export default OshCultureReport;
