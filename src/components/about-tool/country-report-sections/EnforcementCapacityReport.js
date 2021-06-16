060037f1f178e32c29cc5f65b1f7d50217879aec
David Prado <dprado@bilbomatica.es>
2021-06-16 12:19:54 +0200

    Fixed issue that caused an error when there is no qualitative data for the selected country

--- src/components/about-tool/country-report-sections/EnforcementCapacityReport.js
+++ src/components/about-tool/country-report-sections/EnforcementCapacityReport.js
@@ -45,8 +45,8 @@ class EnforcementCapacityReport extends Component
         }
     }
 
-    isDataUndefined = (text) => {
-        if(text === undefined){
+    isDataUndefined = (text, attr) => {
+        if(text === undefined || text[attr] === undefined){
             return <div>{this.props.literals.L20706}</div>
         }else{
             return <div>{ReactHtmlParser(this.props.literals["L"+text])}</div>
@@ -87,31 +87,13 @@ class EnforcementCapacityReport extends Component
                         sector={["14"]}
                         answers={["1","2"]}
                     />
-
-                    {/* TODO Table with data in a different component */}
-                    {/* <table className="w50 chart-data">
-                        <thead>
-                        <tr>
-                            <th>Country</th>
-                            <th>Yes (%)</th>
-                            <th>No (%)</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr data-ng-repeat="row in establishmentsInspected">
-                            <th data-ng-bind="row.country"></th>
-                            <td data-ng-bind="row.yes"></td>
-                            <td data-ng-bind="row.no"></td>
-                        </tr>
-                        </tbody>
-                    </table> */}
                 </div>
 
                 {/* Authority */}
                 <div className="box-rounded content">
                     <h4 className="header4">{this.props.literals.L333}</h4>
                     {/* <div data-ng-if="enforcementCapacities.scope != null">{ReactHtmlParser(this.props.literals["L"+this.state.countryData.text1])}</div> */}
-                    {this.isDataUndefined(this.state.countryData.text1)}
+                    {this.isDataUndefined(this.state.countryData, 'text1')}
                     {/* <div data-ng-if="enforcementCapacities.authority == null" data-ng-bind="i18nLiterals.L20706"></div> */}
                 </div>
 
@@ -119,7 +101,7 @@ class EnforcementCapacityReport extends Component
                 <div className="box-rounded content">
                     <h4 className="header4">{this.props.literals.L334}</h4>
                     {/* <div data-ng-if="enforcementCapacities.scope != null">{ReactHtmlParser(this.props.literals["L"+this.state.countryData.text2])}</div> */}
-                    {this.isDataUndefined(this.state.countryData.text2)}
+                    {this.isDataUndefined(this.state.countryData, 'text2')}
                     {/* <div data-ng-if="enforcementCapacities.scope == null" data-ng-bind="i18nLiterals.L20706"></div> */}
                 </div>
 
@@ -127,7 +109,7 @@ class EnforcementCapacityReport extends Component
                 <div className="box-rounded content inspectorpowers">
                     <h4 className="header4">{this.props.literals.L335}</h4>
                     {/* <div>{ReactHtmlParser(this.props.literals["L"+this.state.countryData.text3])}</div> */}
-                    {this.isDataUndefined(this.state.countryData.text3)}
+                    {this.isDataUndefined(this.state.countryData, 'text3')}
                     {/* <div data-ng-if="enforcementCapacities.inspector == null" data-ng-bind="i18nLiterals.L20706"></div> */}
                 </div>
 
@@ -135,7 +117,7 @@ class EnforcementCapacityReport extends Component
                 <div className="box-rounded content">
                     <h4 className="header4">{this.props.literals.L336}</h4>
                     {/* <div data-ng-if="enforcementCapacities.scope != null">{ReactHtmlParser(this.props.literals["L"+this.state.countryData.text4])}</div> */}
-                    {this.isDataUndefined(this.state.countryData.text4)}
+                    {this.isDataUndefined(this.state.countryData, 'text4')}
                     {/* <div data-ng-if="enforcementCapacities.strategy == null" data-ng-bind="i18nLiterals.L20706"></div> */}
                 </div>
             </div>
