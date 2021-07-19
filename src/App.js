7e76ef9457e9040907715dec76d72fc3e73875dc
manuel-daCosta <manueldacosta@bilbomatica.es>
2021-07-19 11:51:19 +0200

    fix: added control when countries are being set by url, so it erases the secound country

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 
 import './style/App.scss';
 import CookiesComponent from './components/common/cookies/CookiesComponent';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCountry2 } from './actions';
 
 const App = (props) => 
 {
+	const { selectCountry, selectCountry2 } = useSelector((state) => state.selectCountries);
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		if (selectCountry == selectCountry2) {
+			dispatch(setCountry2(''))
+		}
+	}, [selectCountry, selectCountry2])
+
 	// props.children.type.displayName will contain the name of the component that will be painted between the Header and the Footer
 	let child = props.children.type.displayName;
 	// When using mapStateToProps, the displayName of the component changes
