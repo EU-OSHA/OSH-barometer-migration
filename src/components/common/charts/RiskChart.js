ec9b5050db1a9a873f4ca802fb2edbdd4a3e5116
David Prado <dprado@bilbomatica.es>
2021-07-28 14:26:10 +0200

    Solved issue with Overall Opinion - Health at Risk chart being the same when changing the country in the Full Country Repor

--- src/components/common/charts/RiskChart.js
+++ src/components/common/charts/RiskChart.js
@@ -240,7 +240,8 @@ class Chart extends Component {
 	}
 
 	componentDidUpdate(prevProps, prevState ) {
-
+		if (!this.props.fullCountryReport)
+		{
 			if (prevState.select != this.state.select){
 				if (this.state.select == 'sector'){
 				this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1, this.props.selectCountry2, this.props.sector)	
@@ -251,9 +252,8 @@ class Chart extends Component {
 				if (this.state.select == 'age'){
 				this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1, this.props.selectCountry2, null, null,this.props.age);
 				}
-			}
-			
-
+			}			
+	
 			if (this.state.select == 'sector'){
 				if (prevProps.selectCountry1 != this.props.selectCountry1) {
 					this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1, this.props.selectCountry2,this.props.sector)
@@ -262,7 +262,7 @@ class Chart extends Component {
 					this.getLoadDataRisk(this.props.chart,this.props.indicator,this.props.selectCountry1,this.props.selectCountry2,this.props.sector)
 				}
 			}
-
+	
 			if (this.state.select == 'gender'){
 				if (prevProps.selectCountry1 != this.props.selectCountry1) {
 					this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1, this.props.selectCountry2,null,this.props.gender)
@@ -271,8 +271,7 @@ class Chart extends Component {
 					this.getLoadDataRisk(this.props.chart,this.props.indicator,this.props.selectCountry1,this.props.selectCountry2,null,this.props.gender)
 				}
 			}
-
-		
+			
 			if (this.state.select == 'age'){
 				if (prevProps.selectCountry1 != this.props.selectCountry1) {
 					this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1, this.props.selectCountry2,null,null,this.props.age)
@@ -281,19 +280,23 @@ class Chart extends Component {
 					this.getLoadDataRisk(this.props.chart,this.props.indicator,this.props.selectCountry1,this.props.selectCountry2,null,null,this.props.age)
 				}
 			}
-
-			
-
-		// if (prevProps.selectCountry1 != this.props.selectCountry1) {
-		// 	this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1, this.props.selectCountry2,null,null,this.props.age)
-		// }
-		// if (prevProps.selectCountry2 != this.props.selectCountry2) {
-		// 	this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1,this.props.selectCountry2,null,null,this.props.age)
-		// }		
-
+		}
+		else
+		{
+			if (this.props.selectCountry1 != prevProps.selectCountry1)
+			{
+				if (this.props.selectedIndicator == 'sector'){
+					this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1, this.props.selectCountry2, this.props.sector)	
+				}
+				if (this.props.selectedIndicator == 'gender'){
+					this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1, this.props.selectCountry2, null, this.props.gender);
+				}
+				if (this.props.selectedIndicator == 'age'){
+					this.getLoadDataRisk(this.props.chart, this.props.indicator, this.props.selectCountry1, this.props.selectCountry2, null, null,this.props.age);
+				}
+			}
+		}
 	}
-
-
 
 	render() {
 		let selectDiv;
